bc0f336a0d99b740dbf8a2f6a7557ab4aab61840
PelleFre <pelle.freden99@gmail.com>
2023-03-29 15:24:49 +0200

    project initiate

---
+++ UIP2_Game/src/uip2_game/UIP2_Game.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uip2_game;
+
+/**
+ *
+ * @author per-emilfreden
+ */
+public class UIP2_Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
